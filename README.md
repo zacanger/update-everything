6b2a6babf90bf4624d8b35064e3142a70fc06d10
zacanger <zac@zacanger.com>
2016-10-25 21:11:21 -0600

    it werkz

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 Update all global modules.
 
-Coming _very_ soon.
+Installation: `npm i -g update-everything`
+
+Usage: `update-everything`
+
+This will update ALL your globally installed modules.
+This may take quite a while. And some stuff might break.
+This will prompt you to be sure you want to do this.
+
+Depends heavily on [global-packages](https://github.com/zeit/global-packages)
+and, of course, [npm](https://npmjs.com).
+
+Also check out [global-packages-cli](https://npmjs.com/package/global-packages-cli),
+to list your globally-installed packages in text or JSON.
+
+License: WTFPL
--- index.js
+++ index.js
@@ -2,12 +2,49 @@
 
 const npm = require('npm')
 const gp = require('global-packages')
+const { createInterface } = require('readline')
+const { log } = console
+const query = `
+  This will update ALL your globally installed modules.
+  This may take quite a while. And some stuff might break.
+  Are you sure you want to do this?
+`
 
-gp().then((a) => {
-  a.forEach((i) => {
-    npm.load({ global: true }, (err, npm) => {
-      if (err) return console.warn('Error!', err)
-      npm.commands.install([i])
+// adapted from create-react-app's prompt
+const termPrompt = (question, isYesDefault) => {
+  return new Promise(resolve => {
+    const rlInterface = createInterface({
+      input  : process.stdin
+    , output : process.stdout
+    })
+
+    const hint = isYesDefault ? '[Y/n]' : '[y/N]'
+    const message = `${question} ${hint}\n`
+
+    rlInterface.question(message, answer => {
+      rlInterface.close()
+
+      const useDefault = answer.trim().length === 0
+      if (useDefault) return resolve(isYesDefault)
+
+      const isYes = answer.match(/^(yes|y)$/i)
+      return resolve(isYes)
     })
   })
+}
+
+termPrompt(query).then((sure) => {
+  if (sure) {
+    gp().then((a) => {
+      a.forEach((i) => {
+        npm.load({ global: true }, (err, npm) => {
+          if (err) return console.warn('Error!', err)
+          console.log(`Installing ${i}`)
+          npm.commands.install([i])
+        })
+      })
+    })
+  } else {
+    log('Okay, see you next time!')
+  }
 })
